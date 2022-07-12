27707ffa3f753953a60f7c1b7d1e4ee489e578c2
kellettemma <emma.kellett@gmail.com>
2022-07-12 20:28:19 +0100

    t

---
+++ build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+
+// Do not edit directly
+// Generated on Tue, 12 Jul 2022 19:27:50 GMT
+
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorBaseGrayLight,
+ColorBaseGrayMedium,
+ColorBaseGrayDark,
+ColorBaseRed,
+ColorBaseGreen,
+ColorFontBase,
+ColorFontSecondary,
+ColorFontTertiary
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Tue, 12 Jul 2022 19:27:50 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
