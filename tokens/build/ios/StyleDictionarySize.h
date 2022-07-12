dcb8fe15a14c017e0e84dca0baa5251780eb6646
kellettemma <emma.kellett@gmail.com>
2022-07-12 19:51:22 +0100

    add tokens

---
+++ tokens/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Tue, 12 Jul 2022 18:49:41 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
